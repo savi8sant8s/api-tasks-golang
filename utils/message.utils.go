8b86c21d9391d46a968c8d4dae92de20c0de83c5
Savio Santos <saviosa08@gmail.com>
2021-10-11 09:38:12 -0300

    :recycle: Refat api messages

--- utils/message.utils.go
+++ utils/message.utils.go
@@ -1,25 +1,27 @@
 package utils
 
-const BLANK = ""
+const (
+	BLANK = ""
 
-const EMAIL_ALREADY_REGISTERED = "Já existe um usuário cadastrado com esse email."
-const EMAIL_NOT_REGISTERED = "Não existe usuário cadastrado com esse email."
-const USER_REGISTERED = "Usuário cadastrado com sucesso."
-const INVALID_CREDENTIALS = "As credenciais informadas estão incorretas."
-const LOGIN_SUCCESS = "Login realizado com sucesso"
-const LOGOUT_SUCCESS = "Logout realizado com sucesso"
+	EMAIL_ALREADY_REGISTERED = "Já existe um usuário cadastrado com esse email."
+	EMAIL_NOT_REGISTERED = "Não existe usuário cadastrado com esse email."
+	USER_REGISTERED = "Usuário cadastrado com sucesso."
+	INVALID_CREDENTIALS = "As credenciais informadas estão incorretas."
+	LOGIN_SUCCESS = "Login realizado com sucesso"
+	LOGOUT_SUCCESS = "Logout realizado com sucesso"
 
-const ERROR_NAME = "O nome deve possuir de 3 a 34 letras."
-const ERROR_EMAIL = "O email deve seguir o padrao fulano@email.com."
-const ERROR_PASSWORD = "A senha deve possuir de 8 a 32 caracteres."
-const ERROR_INCORRECT_AUTH_HEADER = "Token não especificado corretamente."
-const ERROR_INVALID_TOKEN = "Token inválido ou sessão expirada."
+	ERROR_NAME = "O nome deve possuir de 3 a 34 letras."
+	ERROR_EMAIL = "O email deve seguir o padrao fulano@email.com."
+	ERROR_PASSWORD = "A senha deve possuir de 8 a 32 caracteres."
+	ERROR_INCORRECT_AUTH_HEADER = "Token não especificado corretamente."
+	ERROR_INVALID_TOKEN = "Token inválido ou sessão expirada."
 
-const ERROR_TASK_TITLE = "O título da tarefa deve possuir de 3 a 20 caracteres."
-const ERROR_TASK_MESSAGE = "A mensagem da tarefa deve possuir de 3 a 100 caracteres."
+	ERROR_TASK_TITLE = "O título da tarefa deve possuir de 3 a 20 caracteres."
+	ERROR_TASK_MESSAGE = "A mensagem da tarefa deve possuir de 3 a 100 caracteres."
 
-const CREATE_TASK_SUCCESS = "Tarefa criada com sucesso."
-const ALL_TASKS_SUCCESS = "Todas as tarefas retornadas com sucesso."
-const DELETE_TASK_SUCCESS = "Tarefa deletada com sucesso."
-const UPDATE_TASK_SUCCESS = "Tarefa alterada com sucesso."
-const NOT_FOUND_TASK_SUCCESS = "Tarefa não encontrada com esse ID."
+	CREATE_TASK_SUCCESS = "Tarefa criada com sucesso."
+	ALL_TASKS_SUCCESS = "Todas as tarefas retornadas com sucesso."
+	DELETE_TASK_SUCCESS = "Tarefa deletada com sucesso."
+	UPDATE_TASK_SUCCESS = "Tarefa alterada com sucesso."
+	NOT_FOUND_TASK_SUCCESS = "Tarefa não encontrada com esse ID."
+)
